5fedd52bc67863777597b2f677665f0d5b372fa1
songyanling <songyl157@chinaunicom.cn>
2023-12-07 11:06:07 +0800

    update frontend arch

--- safeguard_web/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Host(models.Model):
-    hostname = models.CharField(max_length=100, primary_key=True)
-    ip = models.CharField(max_length=16)
-    
-    def __str__(self):
-        return self.hostname
