83e279d1129fd7009d9722c010e5021d1788fcf0
ylsong <songyl157@chinaunicom.cn>
2023-12-29 09:43:36 +0800

    add user and group group api

--- backend/models.py
+++ backend/models.py
@@ -6,11 +6,18 @@ class hosts(models.Model):
     password = models.CharField(max_length=50)
     port = models.IntegerField(null=True, blank=True)
     cluster = models.CharField(max_length=50)
+    def __str__(self):
+        return self.ipaddress
+    
 
 class users(models.Model):
     user = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
+    def __str__(self):
+        return self.user
 
 class cluster(models.Model):
     name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
 
--- safeguard_web/settings.py
+++ safeguard_web/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'backend.apps.BackendConfig',
+    'rest_framework',
     'rest_framework_swagger',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -45,6 +46,28 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
 
+SWAGGER_SETTINGS = {
+    # 基础样式
+    'SECURITY_DEFINITIONS': {
+        "basic":{
+            'type': 'basic'
+        }
+    },
+    # 如果需要登录才能够查看接口文档, 登录的链接使用restframework自带的.
+    # 'LOGIN_URL': 'rest_framework:login',
+    # 'LOGOUT_URL': 'rest_framework:logout',
+    # 'DOC_EXPANSION': None,
+    # 'SHOW_REQUEST_HEADERS':True,
+    # 'USE_SESSION_AUTH': True,
+    # 'DOC_EXPANSION': 'list',
+    # 接口文档中方法列表以首字母升序排列
+    'APIS_SORTER': 'alpha',
+    # 如果支持json提交, 则接口文档中包含json输入框
+    'JSON_EDITOR': True,
+    # 方法列表字母排序
+    'OPERATIONS_SORTER': 'alpha',
+    'VALIDATOR_URL': None,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
