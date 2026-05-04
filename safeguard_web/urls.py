9e2a1c331c06e2290cf522fe8ee565b3a5d64574
wangc663 <wangc663@chinaunicom.cn>
2026-05-04 11:48:37 +0800

    feat(db): update readme.md

--- safeguard_web/urls.py
+++ safeguard_web/urls.py
@@ -3,7 +3,6 @@ from django.urls import path,include
 from rest_framework import routers
 from backend import views
 from rest_framework.schemas import get_schema_view
-# from rest_framework_swagger.renderers import SwaggerUIRenderer, OpenAPIRenderer
 from drf_yasg.renderers import SwaggerUIRenderer, OpenAPIRenderer
 
 schema_view = get_schema_view(title='API', renderer_classes=[OpenAPIRenderer, SwaggerUIRenderer])
