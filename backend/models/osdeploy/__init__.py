ce7ce2063819955724c4b93d56825205d803306f
wangc663 <wangc663@chinaunicom.cn>
2026-05-11 14:30:52 +0800

    feat(osdeploy: define kickstart status model

--- backend/models/osdeploy/__init__.py
+++ backend/models/osdeploy/__init__.py
@@ -1,5 +1,6 @@
 from backend.models.osdeploy.job_status import JobStatus
 from backend.models.osdeploy.repo_status import RepoStatus
 from backend.models.osdeploy.pxe_server_status import PXEServerStatus
+from backend.models.osdeploy.kickstart_file_status import KickStartFileStatus
 
-__all__ = ['JobStatus', 'RepoStatus', 'PXEServerStatus']
+__all__ = ['JobStatus', 'RepoStatus', 'PXEServerStatus', 'KickStartFileStatus']
