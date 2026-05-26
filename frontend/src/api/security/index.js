605e72e31b0db35b0e75f7dc04ee88a26b118c15
wangc663 <wangc663@chinaunicom.cn>
2026-05-26 10:14:23 +0800

    feat: add security index

---
+++ frontend/src/api/security/index.js
@@ -0,0 +1,40 @@
+/** Safeguard API */
+import api from '../index'
+
+export function getSafeguards(params) {
+  return api.get('/safeguards/', { params })
+}
+
+export function getSafeguard(id) {
+  return api.get(`/safeguards/${id}/`)
+}
+
+export function createSafeguard(data) {
+  return api.post('/safeguards/', data)
+}
+
+export function updateSafeguard(id, data) {
+  return api.put(`/safeguards/${id}/`, data)
+}
+
+export function deleteSafeguard(id) {
+  return api.delete(`/safeguards/${id}/`)
+}
+
+export function deploySafeguard(id) {
+  return api.post(`/safeguards/${id}/deploy/`)
+}
+
+export function rollbackSafeguard(id) {
+  return api.post(`/safeguards/${id}/rollback/`)
+}
+
+export default {
+  getSafeguards,
+  getSafeguard,
+  createSafeguard,
+  updateSafeguard,
+  deleteSafeguard,
+  deploySafeguard,
+  rollbackSafeguard,
+}
