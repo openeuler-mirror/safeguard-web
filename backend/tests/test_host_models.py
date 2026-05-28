4cf35538ff7fae9173ee5eaa44a3bbc1b88cb562
wangc663 <wangc663@chinaunicom.cn>
2026-05-28 09:43:33 +0800

    feat: update image modle ut

--- backend/tests/test_host_models.py
+++ backend/tests/test_host_models.py
@@ -247,3 +247,93 @@ class VMModelTest(TestCase):
         vm_id = vm.id
         self.host.delete()
         self.assertFalse(VM.objects.filter(id=vm_id).exists())
+
+    def test_vm_extended_fields(self):
+        """测试VM扩展字段"""
+        vm = VM.objects.create(
+            name='extended-vm',
+            uuid='uuid-extended',
+            host=self.host,
+            imageid='img-001',
+            sysdisk={'type': 'qcow2', 'size': '100G'},
+            datadisk=[{'type': 'qcow2', 'path': '/data/disk.qcow2'}],
+            status_message='VM is running normally',
+            vm_image_path='/var/lib/libvirt/images/extended.qcow2',
+            vm_disk_path='/var/lib/libvirt/images/extended-disk.qcow2',
+            vm_network_bridge='mgmt'
+        )
+        self.assertEqual(vm.imageid, 'img-001')
+        self.assertEqual(vm.sysdisk, {'type': 'qcow2', 'size': '100G'})
+        self.assertEqual(vm.datadisk, [{'type': 'qcow2', 'path': '/data/disk.qcow2'}])
+        self.assertEqual(vm.status_message, 'VM is running normally')
+        self.assertEqual(vm.vm_image_path, '/var/lib/libvirt/images/extended.qcow2')
+        self.assertEqual(vm.vm_disk_path, '/var/lib/libvirt/images/extended-disk.qcow2')
+        self.assertEqual(vm.vm_network_bridge, 'mgmt')
+
+    def test_vm_default_extended_fields(self):
+        """测试VM扩展字段默认值"""
+        vm = VM.objects.create(
+            name='default-extended',
+            uuid='uuid-default-extended',
+            host=self.host
+        )
+        self.assertEqual(vm.imageid, '')
+        self.assertEqual(vm.sysdisk, {})
+        self.assertEqual(vm.datadisk, [])
+        self.assertEqual(vm.status_message, '')
+
+
+class ImageModelTest(TestCase):
+    """Image 模型测试"""
+
+    def setUp(self):
+        self.cluster = Cluster.objects.create(name='ImageCluster')
+        self.host = Host.objects.create(
+            hostname='image-host',
+            ip_address='192.168.1.60',
+            username='root',
+            cluster=self.cluster
+        )
+
+    def test_create_image(self):
+        """测试创建镜像"""
+        image = Image.objects.create(
+            id='img-001',
+            name='centos7.qcow2',
+            ostype='centos',
+            path='/var/lib/libvirt/images/centos7.qcow2',
+            host=self.host
+        )
+        self.assertEqual(image.id, 'img-001')
+        self.assertEqual(image.name, 'centos7.qcow2')
+        self.assertEqual(image.ostype, 'centos')
+        self.assertEqual(image.path, '/var/lib/libvirt/images/centos7.qcow2')
+        self.assertEqual(image.host, self.host)
+
+    def test_image_str(self):
+        """测试镜像字符串表示"""
+        image = Image(id='img-test', name='test-image', ostype='ubuntu')
+        self.assertEqual(str(image), 'test-image (ubuntu)')
+
+    def test_image_host_relation(self):
+        """测试镜像与宿主机的关系"""
+        image = Image.objects.create(
+            id='img-host-relation',
+            name='relation-test',
+            path='/test/path',
+            host=self.host
+        )
+        self.assertEqual(image.host, self.host)
+        self.assertIn(image, self.host.image_set.all())
+
+    def test_image_cascade_delete(self):
+        """测试删除宿主机时镜像也被删除"""
+        image = Image.objects.create(
+            id='img-cascade',
+            name='cascade-test',
+            path='/test/path',
+            host=self.host
+        )
+        image_id = image.id
+        self.host.delete()
+        self.assertFalse(Image.objects.filter(id=image_id).exists())
