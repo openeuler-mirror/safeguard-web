7132829077af7a0474343d4def5496aab83641e5
wangc663 <wangc663@chinaunicom.cn>
2026-06-01 09:57:27 +0800

    feat: add task front

---
+++ frontend/src/api/task.js
@@ -0,0 +1,24 @@
+import api from './index'
+
+export function getTasks(params) {
+  return api.get('/tasks/', { params })
+}
+
+export function getTask(id) {
+  return api.get(`/tasks/${id}/`)
+}
+
+export function queryTasks(data) {
+  return api.post('/tasks/query/', data)
+}
+
+export function pageTasks(data, params) {
+  return api.post('/tasks/page/', data, { params })
+}
+
+export default {
+  getTasks,
+  getTask,
+  queryTasks,
+  pageTasks,
+}
