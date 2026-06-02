e0df9e5cfbe169c04f9d4d0f861abb76f47de6c2
wangc663 <wangc663@chinaunicom.cn>
2026-06-02 10:21:52 +0800

    feat: init migrate

--- backend/services/__init__.py
+++ backend/services/__init__.py
@@ -3,6 +3,7 @@ from backend.services.user import UserService, UserAuthorityService
 from backend.services.authority import AuthorityService, MenuService
 from backend.services.host import ClusterService, HostService, VMService
 from backend.services.task import TaskService
+from backend.services.osmigrate import X2cuService
 
 __all__ = [
     'UserService',
@@ -13,4 +14,5 @@ __all__ = [
     'HostService',
     'VMService',
     'TaskService',
+    'X2cuService',
 ]
