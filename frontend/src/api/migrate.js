9b16f8d1540202fc31bfde1ed74c29a7b4f50815
wangc663 <wangc663@chinaunicom.cn>
2026-06-02 11:02:21 +0800

    feat: add front migrate file

---
+++ frontend/src/api/migrate.js
@@ -0,0 +1,34 @@
+import api from './index'
+
+export function getMigrates(params) {
+  return api.get('/migrates/', { params })
+}
+
+export function getMigrate(id) {
+  return api.get(`/migrates/${id}/`)
+}
+
+export function createMigrateInit(data) {
+  return api.post('/migrates/init/', data)
+}
+
+export function createMigrate(data) {
+  return api.post('/migrates/migrate/', data)
+}
+
+export function createMigrateBack(data) {
+  return api.post('/migrates/back/', data)
+}
+
+export function getMigrateStatus(id) {
+  return api.get(`/migrates/${id}/status/`)
+}
+
+export default {
+  getMigrates,
+  getMigrate,
+  createMigrateInit,
+  createMigrate,
+  createMigrateBack,
+  getMigrateStatus,
+}
