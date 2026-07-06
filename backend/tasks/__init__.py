671c98a0cc5a631de6f74b6df0f8b5504a78a49f
wangc663 <wangc663@chinaunicom.cn>
2026-07-06 16:47:49 +0800

    feat: add safegaurd tasks by celery

--- backend/tasks/__init__.py
+++ backend/tasks/__init__.py
@@ -2,8 +2,10 @@
 from .security import deploy_safeguard, rollback_safeguard
 from .osdeploy import auto_install_os
 from .osmigrate import migrate_init_task, migrate_task, migrate_back_task
+from .safeguard import apply_policy_task, rollback_policy_task, collect_file_monitor_events
 
 __all__ = [
     "deploy_safeguard", "rollback_safeguard", "auto_install_os",
     "migrate_init_task", "migrate_task", "migrate_back_task",
+    "apply_policy_task", "rollback_policy_task", "collect_file_monitor_events",
 ]
