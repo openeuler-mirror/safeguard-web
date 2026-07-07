e74049d76f33f2457ae4782a1c51194b5ed76146
wangc663 <wangc663@chinaunicom.cn>
2026-07-07 14:26:23 +0800

    feat: define error types

---
+++ backend/common/exceptions.py
@@ -0,0 +1,106 @@
+"""
+自定义异常类
+
+用于服务层抛出业务异常，包含错误码和错误信息。
+"""
+from backend.common.errcodes import ErrCode, get_errmsg
+
+
+class ServiceError(Exception):
+    """服务层基础异常"""
+
+    def __init__(self, err_code: int, err_msg: str = None):
+        self.err_code = err_code
+        self.err_msg = err_msg or get_errmsg(err_code)
+        super().__init__(self.err_msg)
+
+
+class HostNotFoundError(ServiceError):
+    """主机不存在"""
+
+    def __init__(self, host_id: int):
+        super().__init__(ErrCode.HOST_NOT_FOUND, f"主机 {host_id} 不存在")
+
+
+class VMNotFoundError(ServiceError):
+    """虚拟机不存在"""
+
+    def __init__(self, vm_id: int):
+        super().__init__(ErrCode.HOST_NOT_FOUND, f"虚拟机 {vm_id} 不存在")
+
+
+class ClusterNotFoundError(ServiceError):
+    """集群不存在"""
+
+    def __init__(self, cluster_id: int):
+        super().__init__(ErrCode.HOST_NOT_FOUND, f"集群 {cluster_id} 不存在")
+
+
+class PolicyTemplateNotFoundError(ServiceError):
+    """策略模板不存在"""
+
+    def __init__(self, template_id: int):
+        super().__init__(ErrCode.NOT_FOUND, f"策略模板 {template_id} 不存在")
+
+
+class HostPolicyNotFoundError(ServiceError):
+    """主机策略不存在"""
+
+    def __init__(self, host_id: int):
+        super().__init__(ErrCode.NOT_FOUND, f"主机 {host_id} 的策略不存在")
+
+
+class TaskNotFoundError(ServiceError):
+    """任务不存在"""
+
+    def __init__(self, task_id: int):
+        super().__init__(ErrCode.NOT_FOUND, f"任务 {task_id} 不存在")
+
+
+class JobNotFoundError(ServiceError):
+    """任务不存在（用于 OS Deploy）"""
+
+    def __init__(self, job_id: str):
+        super().__init__(ErrCode.NOT_FOUND, f"任务不存在: {job_id}")
+
+
+class HardwareCollectError(ServiceError):
+    """硬件信息采集失败"""
+
+    def __init__(self, message: str):
+        super().__init__(ErrCode.HOST_HARDWARE_COLLECT_FAILED, message)
+
+
+class LLDCollectError(ServiceError):
+    """LLDP 信息采集失败"""
+
+    def __init__(self, message: str):
+        super().__init__(ErrCode.HOST_LLDP_COLLECT_FAILED, message)
+
+
+class PasswordUpdateError(ServiceError):
+    """密码更新失败"""
+
+    def __init__(self, message: str):
+        super().__init__(ErrCode.HOST_PASSWORD_UPDATE_FAILED, message)
+
+
+class VMOperationError(ServiceError):
+    """虚拟机操作失败"""
+
+    def __init__(self, message: str):
+        super().__init__(ErrCode.VM_OPERATION_FAILED, message)
+
+
+class OperationError(ServiceError):
+    """通用操作失败异常"""
+
+    def __init__(self, message: str):
+        super().__init__(ErrCode.OPERATION_FAILED, message)
+
+
+class HostConnectionError(ServiceError):
+    """主机连接失败"""
+
+    def __init__(self, message: str):
+        super().__init__(ErrCode.OPERATION_FAILED, message)
